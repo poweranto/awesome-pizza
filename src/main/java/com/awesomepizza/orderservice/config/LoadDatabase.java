0ab9e8034067d428fbb40f2fcbeafddaa0c0689f
poweranto <antonio.costantini@reload.it>
2025-09-08 17:05:44 +0200

    load database with some examples data

--- src/main/java/com/awesomepizza/orderservice/config/LoadDatabase.java
+++ src/main/java/com/awesomepizza/orderservice/config/LoadDatabase.java
@@ -22,13 +22,23 @@ public class LoadDatabase {
 		}
 		
 		return args -> {
-			Pizza p1 = new Pizza("Margherita", "pomodoro, mozzarella, basilico");
-			Pizza p2 = new Pizza("Diavola", "pomodoro, mozzarella, basilico, salame piccante");
-			Pizza p3 = new Pizza("Capricciosa", "pomodoro, mozzarella, funghi, prosciutto cotto, carciofini e olive");
+			Pizza p1 = new Pizza("Margherita", "pomodoro, mozzarella e basilico");
+			Pizza p2 = new Pizza("Marinara", "pomodoro, aglio e origano");
+			Pizza p3 = new Pizza("Quattro Stagioni", "pomodoro, mozzarella, funghi, prosciutto cotto, carciofini e olive");
+			Pizza p4 = new Pizza("Quattro Formaggi", "mozzarella, gorgonzola, fontina e parmigiano");
+			Pizza p5 = new Pizza("Capricciosa", "pomodoro, mozzarella, funghi, prosciutto cotto, carciofini e olive");
+			Pizza p6 = new Pizza("Diavola", "pomodoro, mozzarella e salame piccante");
+			Pizza p7 = new Pizza("Regina", "pomodoro, mozzarella, prosciutto cotto e funghi champignon");
+			Pizza p8 = new Pizza("Napoli", "pomodoro, mozzarella, acciughe e origano");
 			
 			pizzaRepository.save(p1);
 			pizzaRepository.save(p2);
 			pizzaRepository.save(p3);
+			pizzaRepository.save(p4);
+			pizzaRepository.save(p5);
+			pizzaRepository.save(p6);
+			pizzaRepository.save(p7);
+			pizzaRepository.save(p8);
 		};
 	}
 }
