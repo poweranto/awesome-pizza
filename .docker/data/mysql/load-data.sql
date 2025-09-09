7ecb3febde140659e194a9746c3ec42e65d0b496
poweranto <antonio.costantini@reload.it>
2025-09-09 12:21:18 +0200

    remove db initialization data script

--- .docker/data/mysql/load-data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `database`.pizzas (id, description, name) VALUES(1, 'pomodoro, mozzarella e basilico', 'Margherita');
-INSERT INTO `database`.pizzas (id, description, name) VALUES(2, 'pomodoro, aglio e origano', 'Marinara'); 
-INSERT INTO `database`.pizzas (id, description, name) VALUES(3, 'pomodoro, mozzarella, funghi, prosciutto cotto, carciofini e olive', 'Quattro Stagioni');
-INSERT INTO `database`.pizzas (id, description, name) VALUES(4, 'mozzarella, gorgonzola, fontina e parmigiano', 'Quattro Formaggi');
-INSERT INTO `database`.pizzas (id, description, name) VALUES(5, 'pomodoro, mozzarella, funghi, prosciutto cotto, carciofini e olive', 'Capricciosa');
-INSERT INTO `database`.pizzas (id, description, name) VALUES(6, 'pomodoro, mozzarella e salame piccante', 'Diavola');
-INSERT INTO `database`.pizzas (id, description, name) VALUES(7, 'pomodoro, mozzarella, prosciutto cotto e funghi champignon', 'Regina');
-INSERT INTO `database`.pizzas (id, description, name) VALUES(8, 'pomodoro, mozzarella, acciughe e origano', 'Napoli');
